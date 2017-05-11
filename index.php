c0164cf495e3ae0a7e88bcd55ed1da295b107e28
Ozodrukh <ozik.dev@gmail.com>
2017-05-11 09:19:20 +0500

    cors update

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ foreach ($routers as $path => $router) {
 $dispatcher->options('*', function (Request $req, Response $res, Chain $chain) {
     // Add CORS support
     $res->setHeader("Access-Control-Allow-Origin", "*");
-    $res->setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin");
+    $res->setHeader("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Access-Control-Allow-Origin");
     $res->setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 
     $res->send("OK");
